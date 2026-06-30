f1f7efe8b76ee21e1885e8fb29c01f96a9da19e3
dasom222g <dasom228@gmail.com>
2026-06-30 10:34:23 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// 주석
+console.log("멀티캠퍼스 강의중...");
+
+// 변수
+let userName = "후츠릿";
+// console.log("userName", userName);
+
+// 미션:
+// 적절한 변수명에 여러분의 이름을 담아서 변수 만들기
+// console에 찍어서 확인하기
+const userName2 = "후츠릿";
+
+userName = "kjajsdkflj";
+console.log("🚀 ~ userName:", userName);
+
+// 데이터 타입
+const userAge = 33; // 숫자
+const userMBTI = "ENTJ"; // 문자열
+console.log("🚀 ~ userMBTI:", userMBTI);
+
+// boolean타입
+const isLoggedin = false;
+console.log("🚀 ~ isLoggedin:", isLoggedin);
+console.log("Type isLoggedin", typeof isLoggedin);
+
+// null/undefined
+const empty = null;
+let empty2; // 변수 선언만
+console.log("🚀 ~ empty2:", empty2);
+
+// 미션: 나머지 변수들도 타입확인하는 코드 작성해서 콘솔에 찍어보기
